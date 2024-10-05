5271cc73557f32c334eb16bb535341cb68fe4eb0
smrznuti1 <filipbozovic998@gmail.com>
2024-10-05 10:04:53 +0200

    Format

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
-terraform{
-	required_providers{
-		aws = {
-			source = "hashicorp/aws"
-			version = "~> 3.0"
-		}
-	}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
 
 provider "aws" {
-	region = "eu-north-1"
+  region = "eu-north-1"
 }
 
-resource "aws_vpc" "custom_vpc"{
-	cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "custom_vpc" {
+  cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_subnet" "custom_subnet" {
@@ -24,6 +24,6 @@ resource "aws_subnet" "custom_subnet" {
 }
 
 resource "aws_instance" "MyAWSResource" {
-   ami             = "ami-08eb150f611ca"
-   instance_type   = "t2.micro"
+  ami           = "ami-08eb150f611ca"
+  instance_type = "t2.micro"
 }
