3ce7fe0f6b5954ac6b16fb2b6a725a283f2ff5b6
smrznuti1 <filipbozovic998@gmail.com>
2024-10-06 16:57:06 +0200

    Feat: IAM Role creation for Lambda Service

--- Task-2/emailer.ps1
+++ Task-2/emailer.ps1
@@ -1,6 +1,24 @@
 $region = "us-east-1"
 $snsTopicName = "emailer"
-$emailAddress = "filipbozovic998@gmail.com"
+$lambdaIAMRoleName = "emailer-lambda-role"
 
 $snsTopic = New-SNSTopic -Name $snsTopicName -Region $region
-$snsTopic
+Write-Host("SNS Topic ARN: " + $snsTopic)
+
+$rolePolicy = @"
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+"@
+
+$roleArn = New-IamRole -RoleName $lambdaIAMRoleName -AssumeRolePolicyDocument $rolePolicy -Region $region
+Write-Host("IAM Role ARN: " + $roleArn)
