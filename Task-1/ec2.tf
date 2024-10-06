79d3c593c52883b1728d9749fa54ba3c35e1b536
smrznuti1 <filipbozovic998@gmail.com>
2024-10-06 15:37:30 +0200

    Feat: Add key pair for ssh conenction

--- Task-1/ec2.tf
+++ Task-1/ec2.tf
@@ -1,9 +1,15 @@
+resource "aws_key_pair" "custom_key_pair" {
+  key_name   = "custom-key-pair"
+  public_key = file("/home/filip/.ssh/aws-ec2-key.pub")
+}
+
 resource "aws_instance" "MyEC2Instance" {
   ami                         = "ami-0866a3c8686eaeeba"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.custom_subnet.id
   associate_public_ip_address = true
   security_groups             = [aws_security_group.custom_security_group.id]
+  key_name                    = aws_key_pair.custom_key_pair.key_name
   tags = {
     Name        = "test-ec2"
     Description = "Test instance"
@@ -23,7 +29,7 @@ resource "aws_instance" "MyEC2Instance" {
               CREATE TABLE IF NOT EXISTS ${var.table_name} (
                 id SERIAL PRIMARY KEY,
                 name VARCHAR(100),
-                surname VARCHAR(100),
+                surname VARCHAR(100)
               );
               SQL
 
