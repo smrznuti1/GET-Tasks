bfd0eb36a2da0770cad1122922d1de8c7023b005
SESA719972 <filip.bozovic@non.se.com>
2024-10-07 17:09:31 +0200

    Feat: Extract number

--- Task-3/script.sh
+++ Task-3/script.sh
@@ -3,8 +3,14 @@
 regex="k[0-9a-f]{8}\.kod"
 
 while IFS= read -r line; do
-	if [[ $line =~ $regex ]]; then
-		echo "$line"
-	fi
-done < ../prilog.txt
+	touch "$line"
 
+	if ! [[ $line =~ $regex ]]; then
+		echo "$line"
+	else
+		num=${line:7:1}
+		echo "$num in $line"
+	fi
+	
+
+done < ../prilog.txt
