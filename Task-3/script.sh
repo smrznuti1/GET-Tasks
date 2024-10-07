315ca324cc3ed239fe3ddf753a909d41d0fb1af6
SESA719972 <filip.bozovic@non.se.com>
2024-10-07 17:27:23 +0200

    Feat: Even and Odd directories

--- Task-3/script.sh
+++ Task-3/script.sh
@@ -17,6 +17,11 @@ while IFS= read -r line; do
 		if (( numG % 2 == 0 )); then
 			mkdir -p "${letterG}0/${letterE}0"
 			mv "$line" "${letterG}0/${letterE}0"
+		else
+			numG=$((numG - 1))
+			hexG=$(printf "%x\n" $numG)
+			mkdir -p "${hexG}0/${letterE}0"
+			mv "$line" "${hexG}0/${letterE}0"
 		fi
 	fi
 	
